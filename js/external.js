6f92ba5c5852b1e82a5758350e13d393db64c4a5
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-06 21:35:38 -0600

    continuing work on javascript with html exercise

--- js/external.js
+++ js/external.js
@@ -16,23 +16,38 @@ alert("Welcome to my Website!");
 //
 // alert("The total amount due is $" + totalCost + ".");
 
-var googlePay = prompt("How much is Google paying you per hour?");
-var gHours = prompt("How many hours did you work at Google?");
-var amazonPay = prompt("How much is Amazon paying you per hour?");
-var aHours = prompt("How many hours did you work at Amazon?");
-var facebookPay = prompt("How much is Facebook paying you per hour?");
-var fHours = prompt("How many hours did you work at Facebook?");
+// var rentalPriceInDollars = prompt("How much is the cost of a single rental, in dollars?");
+// var littleMermaidRental = prompt("How many days would you like to rent The Little Mermaid?");
+// var brotherBearRental = prompt("How many days would you like to rent Brother Bear?");
+// var herculesRental = prompt("How many days would you like to rent Hercules?");
+// var costOfMovieRentals = alert("You owe $" + (+littleMermaidRental + +brotherBearRental + +herculesRental)*(+rentalPriceInDollars) + ".")
+
+// var googlePay = prompt("How much is Google paying you per hour?");
+// var googleHours = prompt("How many hours did you work at Google this week?");
+// var amazonPay = prompt("How much is Amazon paying you per hour?");
+// var amazonHours = prompt("How many hours did you work at Amazon this week?");
+// var facebookPay = prompt("How much is Facebook paying you per hour?");
+// var facebookHours = prompt("How many hours did you work at Facebook this week?");
+// var weeklyContractorSalary = alert("Your salary for the week is $" + ((+googlePay*+googleHours) + (+amazonPay*+amazonHours) + (+facebookPay*+facebookHours)) + ".");
 
 
 // add plus (+) to turn string into a #
-var classVacancy = confirm("Is the class full?");
 
-var conflict = confirm("Does the class schedule conflict with your current schedule?");
 
-var enroll = ((classVacancy === false) && (conflict === false));
+var classNotVacant = confirm("Is the class full?");
 
-var doNotEnroll = ((classVacancy === true) && (conflict === true)) || ((classVacancy === false) && (conflict === true)) || ((classVacancy === true) && (conflict === false));
+var scheduleConflict = confirm("Does the class schedule conflict with your current schedule?");
 
-enroll = alert("Go ahead and enroll!");
+var enrollStudent = !classNotVacant && !scheduleConflict;
 
-doNotEnroll = alert("Sorry, you cannot enroll at this time.");
+enrollStudent = alert("Go ahead and enroll!");
+
+// var doNotEnroll = ((classVacancy === true) && (conflict === true)) || ((classVacancy === false) && (conflict === true)) || ((classVacancy === true) && (conflict === false));
+
+// doNotEnroll = alert("Sorry, you cannot enroll at this time.");
+
+
+var amountPurchased = prompt("How many items were purchased by the customer?");
+var offerNotExpired = confirm("Is offer still valid? [Offer valid until March 2019]");
+var premiumMembershipVerified = confirm("Is customer a premium member?");
+var applyProductOffer = (amountPurchased > 2 && offerNotExpired) || premiumMembershipVerified
