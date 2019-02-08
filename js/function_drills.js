0a7e272c3981632641c2106c69c4ac14490fdc24
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-08 13:29:49 -0600

    continued work on function drills

--- js/function_drills.js
+++ js/function_drills.js
@@ -22,10 +22,10 @@
     console.log(identity("hello"));
 
     function isFive(input) {
-        return input === 5;
+        return input == 5;
     }
 
-    console.log(isFive(5));
+    console.log("isFive: " + isFive("5"));
 
     function addFive(input) {
         return input + 5;
@@ -34,31 +34,31 @@
     console.log(addFive(4));
 
     function isMultipleOfFive(input) {
-        return input % 5 === 0;
+        return input % 5 == 0;
     }
 
-    console.log(isMultipleOfFive(20));
+    console.log("isMultipleOfFive: " + isMultipleOfFive(20));
 
     function isThree(input) {
-        return input === 3;
+        return input == 3;
     }
 
     console.log(isThree(3));
 
     function isMultipleofThree(input) {
-        return input % 3 === 0;
+        return input % 3 == 0;
     }
 
     console.log(isMultipleofThree(81));
 
     function isMultipleOfThreeAndFive(input) {
-        return input % 3 === 0 && input % 5 === 0;
+        return input % 3 == 0 && input % 5 == 0;
     }
 
-    console.log(isMultipleOfThreeAndFive(30));
+    console.log("isMultipleOfThreeAndFive: " + isMultipleOfThreeAndFive(30));
 
     function isMultipleOf(target, n) {
-        return target % n === 0
+        return target % n == 0
     }
 
     console.log(isMultipleOf(45, 9));
@@ -67,6 +67,65 @@
         return boolean;
     }
 
-    console.log(isTrue());
+    console.log("isTrue: " + isTrue(true));
 
+    function isFalse(boolean) {
+        return boolean;
+    }
+
+    console.log("isFalse: " + isFalse(false));
+
+    function isTruthy(input) {
+        return input === false;
+    }
+
+    console.log("isTruthy: " + isTruthy("go"));
+
+    function isFalsy(input) {
+        return input === true;
+    }
+
+    console.log("isFalsy: " + isFalsy(false));
+
+    function isVowel(letter) {
+        return letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u" || letter === "A" || letter === "E" || letter === "I" || letter === "O" || letter === "U";
+
+
+    console.log("isVowel: " + isVowel("U"));
+
+    // This is not the most efficient method
+
+    // function isConsonant(letter) {
+    //     return letter === "b" || letter === "c" || letter === "d" || letter === "f" ||
+    //     letter === "g" || letter === "h" || letter === "j" || letter === "k" ||
+    //     letter === "l" || letter === "m" || letter === "n" || letter === "p" ||
+    //     letter === "q" || letter === "r" || letter === "s" || letter === "t" ||
+    //     letter === "v" || letter === "w" || letter === "x" || letter === "y" ||
+    //     letter === "z";
+    // }
+    //
+    // console.log("isConsonant: " + isConsonant("i"));
+
+    // This is MUCH MORE efficient
+    function isConsonant(letter) {
+        return !(letter === "a" ||
+            letter === "e" ||
+            letter === "i" ||
+            letter === "o" ||
+            letter === "u");
+    }
+
+    console.log("isConsonant " + isConsonant("a"));
+
+    // This is a prompt for future use...
+    // var letterTest = prompt("Enter a letter.");
+
+    function isCapital(letter) {
+        return letter === letter.toUpperCase();
+    }
+
+    console.log("isCapital: " + isCapital("T"));
+
+    // For use with above prompt...
+    // console.log(letterTest + " isCapital: " + isCapital(letterTest));
 // })();
