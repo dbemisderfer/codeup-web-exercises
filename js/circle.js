1a71c6fb9b20480b35a380929fa1e372bc0a904d
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-17 21:04:17 -0600

    continued work on circle.js exercise

--- js/circle.js
+++ js/circle.js
@@ -8,26 +8,30 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-            var area = Math.PI * radius^2;
+            var area = (Math.PI * Math.pow(circle.radius, 2));
 
             return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-            if (doRounding === true) {
-                doRounding = doMath.round(doRounding);
-            }
 
-            return doRounding;
+            if (doRounding === true) {
+                console.log("Rounded: " + Math.round(circle.getArea()));
+            } else {
+                return console.log("Not rounded: " + circle.getArea());
+            };
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            // console.log("Rounding Area: " + circle.logInfo());
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
         }
     };
 
-    // log info about the circle
+
+    console.log("Rounding Area: " + circle.logInfo());
+    console.log("Area: " + circle.getArea());
+// log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
