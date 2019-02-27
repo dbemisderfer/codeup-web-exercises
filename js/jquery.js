1474dcd4c65453c5e82e86b543b1cd6fdaca70ba
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-27 16:30:25 -0600

    creation of konami exercise

---
+++ js/jquery.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    "use strict";
+
+    var codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+
+    // $(document).keyup(function(event){
+    //     console.log(event.keyCode);
+    // });
+
+    $(document).keydown(function(event) {
+        var enterCode = [];
+        // console.log(event.keyCode);
+        if (event.keyCode === 13 || 37 || 38 || 39 || 40 || 65 || 66) {
+            enterCode.push(event.keyCode);
+            console.log(enterCode);
+        } else {
+            enterCode.pop();
+            console.log(enterCode);
+        }
+
+
+    });
+
+    // $('#textfield').keydown(function(event) {
+    //     var enterCode = [];
+    //     // console.log(event.keyCode);
+    //     if (event.keyCode === 66) {
+    //         alert("You pushed RETURN!")
+    //     }
+    // });
+
+
+
+});
