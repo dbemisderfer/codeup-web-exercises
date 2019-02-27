897050703d798fca51b58276bdaa7c60e2838e53
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-27 12:33:40 -0600

    completion of jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,6 +2,22 @@
 
 $(document).ready(function() {
 
-    alert('The DOM has finished loading!');
+    // alert('The DOM has finished loading!');
+
+    // var contents = $('#container').html();
+    // alert(contents);
+
+    // $('.codeup').css('border', '1px solid #F00');
+
+    $('li').css('font-size', '20px');
+
+    // $('h1').css('background-color', '#FF0');
+    // $('p').css('background-color', '#FF0');
+    // $('li').css('background-color', '#FF0');
+
+    // var contents = $('h1').html();
+    // alert(contents);
+
+    $('h1, p, li').css('background-color', "#FF0");
 
 });
