771f352bff16e62209a2b58f54a09326dc47211f
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-02-28 11:48:55 -0600

    completion of konami exercise

--- js/jquery.js
+++ js/jquery.js
@@ -1,21 +1,58 @@
 $(document).ready(function() {
     "use strict";
 
-    var codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+    // var codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
 
-    // $(document).keyup(function(event){
-    //     console.log(event.keyCode);
-    // });
-    var enterCode = [];
-    $(document).keydown(function(event) {
+    // var codes = "3838404037393739666513";
+
+
+        // var codes = "38384013";
+        //
+        // var enterCode = "";
+        // $(document).keyup(function(event) {
+        //
+        //     enterCode += ("" + event.keyCode);
+        //     // console.log(enterCode);
+        //     if (enterCode === codes) {
+        //         alert('Good Job!');
+        //     }
+        //     // console.log(codes.indexOf(enterCode))
+        //     if (!codes.indexOf(enterCode)) return;
+        //     // console.log(enterCode);
+        //     enterCode = "";
+        //     console.log(enterCode);
+
+
+        var codes = "38384040";
+
+        var enterCode = "";
+
+        $(document).keyup(function(event) {
+
+            enterCode += event.keyCode;
+            console.log(enterCode);
+            if (enterCode === codes) {
+                alert('Good Job!');
+            } else if (!codes.indexOf(enterCode)) {
+                return;
+            }
+            enterCode = "";
+            // // console.log(enterCode);
+
+
+
+
+
+        // if (event.keyCode === 13 || 37 || 38 || 39 || 40 || 65 || 66){
+        //     enterCode.push(event.keyCode);
+        //     console.log(enterCode);
+        // }
 
         // console.log(event.keyCode);
-        if (event.keyCode === 13 || 37 || 38 || 39 || 40 || 65 || 66) {
-            enterCode.push(event.keyCode);
-            console.log(enterCode);
-        } else {
-            enterCode.pop();
-        }
+        // if (event.keyCode !== 13 || 37 || 38 || 39 || 40 || 65 || 66) {
+        //     enterCode = [];
+        //     console.log(enterCode);
+        // }
 
 
     });
@@ -27,7 +64,5 @@ $(document).ready(function() {
     //         alert("You pushed RETURN!")
     //     }
     // });
-
-
 
 });
