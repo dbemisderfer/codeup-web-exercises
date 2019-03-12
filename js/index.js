86bde00d58dc16720a9cdec7dac62d5aa5b857bd
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-03-12 09:53:32 -0500

    final edits after class walkthrough of javascript modules "import" and "require" exercise

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
+// [Import]
 // import sayHello from './say-hello.js';
 
-const sayHello = require('./say-hello.js');
+//Instructor solution [Import]
+// import {sayHello} from './say-hello.js'
+// console.log(sayHello());
+// View in browser console
 
-console.log(sayHello('Hello there.'));
+// OR...
+import banana from './say-hello.js'
+console.log(banana.sayHello());
+// View in browser console
+
+
+
+
+
+
+
+
+
+
+
+// [Require]
+// const sayHello = require('./say-hello.js');
+
+// console.log(sayHello('Hello there.'));
+
+// Instructor solution [Require]
+// const message = require('./say-hello.js');
+// console.log(message.newBinding())
--- js/say-hello.js
+++ js/say-hello.js
@@ -1,5 +1,37 @@
-const sayHello = (input) => input;
+// [Import]
+// const sayHello = (input) => input;
 
+// Instructor solution [Import]
+// export const sayHello = () => {
+//     return "Nice message"
+// }
+
+// OR...
+const sayHello = () => {
+    return "Nice message"
+}
+
+export default {sayHello}
+
+
+
+
+
+
+
+
+
+
+// [Require]
 // export default sayHello;
 
-module.exports = sayHello;
+// module.exports = sayHello;
+
+
+
+// const sayHello = () => {
+//     return "Nice message"
+// }
+//
+// // Instructor solution [Require]
+// module.exports.newBinding = sayHello;
